b283f432da71ccacbc9f3abc1a40841ef42e7518
jjunis <owj1106@gmail.com>
2025-09-16 12:25:11 +0900

    [Feat]이벤트 주문시스템

---
+++ Assets/Scr/DeliveryOrder.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+//간단한 배달 주문
+[System.Serializable]
+public class DeliveryOrder
+{
+    public int orderld;
+    public string restauranName;
+    public string customerName;
+    public Building restauranBuilding;
+    public Building customerBuilding;
+    public float orderTime;
+    public float timeLimit;
+    public float reward;
+    public OrderState state;
+
+    public DeliveryOrder(int id, Building restaurant, Building customer, float rewardAmount)
+    {
+        orderld = id;
+        restauranBuilding = restaurant;
+        customerBuilding = customer;
+        restauranName = restaurant.buildingName;
+        customerName = customer.buildingName;
+        orderTime = Time.time;
+        timeLimit = Random.Range(60f, 120f);            //1~2 분 제한
+        reward = rewardAmount;
+        state = OrderState.WaitingPickup;
+    }
+
+    public float GetRemainingTime()
+    {
+        return Mathf.Max(0f, timeLimit - (Time.time - orderTime));          //남은 시간 리턴
+    }
+
+    public bool IsExpired()
+    {
+        return GetRemainingTime() <= 0f;
+    }
+}
